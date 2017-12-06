2120a4bb1b7c0a133b4a9936ffd53e5263b0ccc6
Willian Lauber <lcwillian_hd@hotmail.com>
2017-12-06 21:58:44 -0200

    sockei Jober

--- src/main/java/com/ferrovia/trens/Main.java
+++ src/main/java/com/ferrovia/trens/Main.java
@@ -1,22 +1,18 @@
 package com.ferrovia.trens;
 
 import com.ferrovia.trens.Trem;
+import com.ferrovia.trens.carga.ClienteTrem;
 import com.ferrovia.trens.estacoes.LinkedStationController;
+import com.ferrovia.trens.estacoes.ServidorDaEsta;
 import com.ferrovia.trens.estacoes.EstacaoController;
 
 public class Main {
-
-    private static final int NO_OF_STATIONS = 8;
-    private static final int NO_OF_TRAINS = 4;
-    private static final Trem[] trains = new Trem[NO_OF_TRAINS];
-
-    public static void main(String[] args) {
-        EstacaoController stationController = new LinkedStationController(NO_OF_STATIONS);
-        for (int i = 0; i < NO_OF_TRAINS; i++) {
-            trains[i] = new Trem(i, 100, i + 1, stationController);
-        }
-        for (int i = 0; i < NO_OF_TRAINS; i++) {
-            new Thread(trains[i]).start();
-        }
-    }
+	  public static void main (String args[ ]) {
+		  
+		 
+		  ServidorDaEsta sd = new ServidorDaEsta();
+		  ClienteTrem ct = new ClienteTrem();
+		  sd.main(null);
+		  ct.main(new String[] {"que passa server?"});
+	  }
 }
